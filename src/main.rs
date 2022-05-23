2dd04451adf85a998b8003f66dbbed8dc6f50977
Denis Suprunenko <chipsoft@gmail.com>
2022-05-23 11:59:46 +0200

    Initial commit. Working LED blinking

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+// use core::convert::TryInto;
+// use rtic::{app, cyccnt::U32Ext};
+// use stm32l4xx_hal::{
+    // prelude::*,
+// };
+
+// fn monotonic_ticks() -> u32 {
+//     app::monotonics::now()
+//         .duration_since_epoch()
+//         .ticks()
+//         .try_into()
+//         .unwrap()
+// }
+
+#[rtic::app(device = stm32l4xx_hal::stm32,
+            dispatchers = [DFSDM1],
+            peripherals = true,
+            )
+]
+
+mod app {
+    use systick_monotonic::fugit::{Duration, RateExtU32};
+    use systick_monotonic::{Systick};
+    use stm32l4xx_hal::prelude::_stm32l4_hal_RccExt;
+    use stm32l4xx_hal::gpio::{GpioExt, Output, PushPull};
+    use stm32l4xx_hal::gpio::PB3;
+    use stm32l4xx_hal::flash::FlashExt;
+    use stm32l4xx_hal::prelude::_stm32l4_hal_PwrExt;
+
+    #[shared]
+    struct Shared {
+    }
+
+    #[local]
+    struct Local {
+        led: PB3<Output<PushPull>>,
+    }
+
+    #[monotonic(binds = SysTick, default = true)]
+    type MonoTimer = Systick<1000>;
+
+    #[init]
+    fn init(c: init::Context) -> (Shared, Local, init::Monotonics) {
+        // This is the `cortex_m::Peripherals` struct without the SysTick which RTIC has taken ownership of.
+        let cp = c.core;
+        // Device access (Peripheral Access Crate)
+        let pac = c.device;
+
+        // Use the HAL to get a pin to control.
+        let mut rcc = pac.RCC.constrain();
+        let mut gpiob = pac.GPIOB.split(&mut rcc.ahb2);
+        let mut flash = pac.FLASH.constrain();
+        let mut pwr = pac.PWR.constrain(&mut rcc.apb1r1);
+
+        let _clocks = rcc
+            .cfgr
+            .sysclk(80.MHz())
+            .pclk1(80.MHz())
+            .pclk2(80.MHz())
+            .freeze(&mut flash.acr, &mut pwr);
+
+        let mut led = gpiob
+            .pb3
+            .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+
+        led.set_low();
+
+        let mono = Systick::new(cp.SYST, 80_000_000);
+
+        // Start the blinky task!
+        blink_led::spawn().unwrap();
+
+        (
+            Shared {  },
+            Local { led },
+            init::Monotonics(mono),
+        )
+    }
+
+    #[task(local = [led, flag: bool = false])]
+    fn blink_led(cx: blink_led::Context) {
+        // Extract the LED
+        let led = cx.local.led;
+        let flag = cx.local.flag;
+
+        if *flag == false {
+            led.set_low();
+            // rprintln!("LED Off");
+        } else {
+            led.set_high();
+            // rprintln!("LED On");
+        }
+
+        blink_led::spawn_after(Duration::<u64, 1, 1000>::from_ticks(500)).unwrap();
+
+        *flag = !*flag;
+    }
+
+}
